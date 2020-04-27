7f4df1e9423315cbbe0455c44999dade52d1b89e
cyril.decaud <cyril.decaud@gmail.com>
2020-04-27 20:08:33 +0200

    Maj CSS + lien header.php avec registration.php

--- HTML/registration.php
+++ HTML/registration.php
@@ -1,3 +1,6 @@
+<?php
+    require_once 'header.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -7,27 +10,29 @@
     </head>
     <body>
         <section>
-            <form class="" action="placeholder.html" method="post">
-                <h2>Information</h2>
-                <div>
-                    <label for="">Gender * </label>
-                    <input type="radio" onclick="" name="value">G
-                    <input type="radio" onclick="" name="value">M
-                    <input type="radio" onclick="" name="value">Other</br>
-                </div>
-                <label for = "rName">Name * </label><input type="text" name="rName" value="" placeholder="Name *"></br>
-                <label for = "rFirstname">Firstname * </label><input type="text" name="rFirstname" value="" placeholder="Firstname *"></br>
-                <label for = "rEmail">Email address * </label><input type="text" name="rEmail" value="" placeholder="Email adress *"></br>
-                <label for = "rPhone">Phone number</label><input type="text" name="rPhone" value="" placeholder="Phone number"></br>
-                <label for = "rBirth">Date of birth * </label><input type="text" name="rBirth" value="" placeholder="Date of birth *"></br>
-                <label for = "rAddress">Address * </label><input type="text" name="rAddress" value="" placeholder="Adress *"></br>
-                <label for = "rZip">ZIP * </label><input type="text" name="rZip" value="" placeholder="ZIP *"> <label for = "rCity">City * </label><input type="text" name="rCity" value="" placeholder="City *"></br>
+            <div id='formUp'>
+                <form class="" action="placeholder.html" method="post" >
+                    <h2>Information</h2>
+                    <div>
+                        <label for="">Gender * </label>
+                        <input type="radio" onclick="" name="value">G
+                        <input type="radio" onclick="" name="value">M
+                        <input type="radio" onclick="" name="value">Other</br>
+                    </div>
+                    <label for = "rName">Name * </label><input type="text" name="rName" value="" placeholder="Name *"></br>
+                    <label for = "rFirstname">Firstname * </label><input type="text" name="rFirstname" value="" placeholder="Firstname *"></br>
+                    <label for = "rEmail">Email address * </label><input type="text" name="rEmail" value="" placeholder="Email adress *"></br>
+                    <label for = "rPhone">Phone number</label><input type="text" name="rPhone" value="" placeholder="Phone number"></br>
+                    <label for = "rBirth">Date of birth * </label><input type="text" name="rBirth" value="" placeholder="Date of birth *"></br>
+                    <label for = "rAddress">Address * </label><input type="text" name="rAddress" value="" placeholder="Adress *"></br>
+                     <label for = "rZip">ZIP * </label><input type="text" name="rZip" value="" placeholder="ZIP *"> <label for = "rCity">City * </label><input type="text" name="rCity" value="" placeholder="City *"></br>
 
-                <h2>Password</h2>
-                <label for = "rPass">Password * </label><input type="password" name="rPass" value="" placeholder="Password *"></br>
-                <label for = "rConfirmPass">Confirm password * </label><input type="password" name="rConfirmPass" value="" placeholder="Confirm password *"></br>
-                <input type="submit" name="cSubmit" value="Confirm">
-            </form>
+                    <h2>Password</h2>
+                    <label for = "rPass">Password * </label><input type="password" name="rPass" value="" placeholder="Password *"></br>
+                    <label for = "rConfirmPass">Confirm password * </label><input type="password" name="rConfirmPass" value="" placeholder="Confirm password *"></br>
+                    <input type="submit" name="cSubmit" value="Confirm">
+                </form>
+            </div>
         </section>
     </body>
 </html>
