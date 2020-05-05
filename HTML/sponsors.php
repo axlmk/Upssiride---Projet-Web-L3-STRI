6ea7cdbbf0cea871961c1c19da726ae04ba525d1
Axel G <axel-gauthier@hotmail.fr>
2020-05-05 18:38:44 +0200

    feat: first attempt to change sponsor syle

--- HTML/sponsors.php
+++ HTML/sponsors.php
@@ -7,12 +7,22 @@
     <head>
         <meta charset="utf-8">
         <title>Sponsors</title>
-        <link rel="stylesheet" href="style/sponsor.css"/>
+        <link rel="stylesheet" href="style/sponsors.css"/>
     </head>
     <body>
-        <h1> They support us : </h1>
+        <h1> They support us: </h1>
 
-        <div id=sponsor1>
+        <div class="tile">
+            <div>
+                <a href="https://stri-online.net/FTLV/">
+                    <img src="Pictures_site/logo_stri.jpg" alt="logo STRI">
+                    <h2>STRI</h2>
+                </a>
+            </div>
+            <p class="description">"Systèmes de Télécommunications & Réseaux Informatiques" is french formation based in Toulouse which teaches network, telecommunication and essentially computer science</p>
+        </div>
+
+        <div class="tile">
 
             <div class=company>
                 <div class=logoComp>
@@ -29,71 +39,4 @@
 
         </div>
 
-        <div id=sponsor1>
-
-            <div class=company>
-                <div class=logoComp>
-                    <a href="https://stri-online.net/FTLV/">
-                            <img src="Pictures_site/logo_stri.jpg" alt="logo STRI">
-                            <h2>STRI</h2>
-                    </a>
-                </div>
-
-                <div class=resumeComp>
-                    <p>"Systèmes de Télécommunications & Réseaux Informatiques" is french formation based in Toulouse which teaches network, telecommunication and essentially computer science</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div id=sponsor1>
-
-            <div class=company>
-                <div class=logoComp>
-                    <a href="https://stri-online.net/FTLV/">
-                            <img src="Pictures_site/logo_stri.jpg" alt="logo STRI">
-                            <h2>STRI</h2>
-                    </a>
-                </div>
-
-                <div class=resumeComp>
-                    <p>"Systèmes de Télécommunications & Réseaux Informatiques" is french formation based in Toulouse which teaches network, telecommunication and essentially computer science</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div id=sponsor1>
-
-            <div class=company>
-                <div class=logoComp>
-                    <a href="https://stri-online.net/FTLV/">
-                            <img src="Pictures_site/logo_stri.jpg" alt="logo STRI">
-                            <h2>STRI</h2>
-                    </a>
-                </div>
-
-                <div class=resumeComp>
-                    <p>"Systèmes de Télécommunications & Réseaux Informatiques" is french formation based in Toulouse which teaches network, telecommunication and essentially computer science</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div id=sponsor2>
-
-            <div class=company>
-                <div class=logoComp>
-                    <a href="https://stri-online.net/FTLV/">
-                            <img src="Pictures_site/logo_stri.jpg" alt="logo STRI">
-                            <h2>STRI</h2>
-                    </a>
-                </div>
-
-                <div class=resumeComp>
-                    <p>"Systèmes de Télécommunications & Réseaux Informatiques" is french formation based in Toulouse which teaches network, telecommunication and essentially computer science</p>
-                </div>
-            </div>
-
-        </div>
     </body>
