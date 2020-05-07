c43da13ba24eafb83c5bde623493017bb705cd18
cyril.decaud <cyril.decaud@gmail.com>
2020-05-07 23:28:53 +0200

    Popup new version sans bug + maj CSS

---
+++ HTML/javascript/modal.js
@@ -0,0 +1,9 @@
+function openModal(){
+    document.getElementById("modal").style.opacity = "1";
+    document.getElementById("modal").style.top = "200px"
+}
+
+function closeModal(){
+    document.getElementById("modal").style.opacity = "0";
+    document.getElementById("modal").style.top = "220px"
+}
