dc8cf28d1f095f129c7ebba0b1c268c166b4ee92
cyril.decaud <cyril.decaud@gmail.com>
2020-05-07 18:22:41 +0200

    Ajout Popup + maj styl.css + my_ride.css

--- HTML/my_rides.php
+++ HTML/my_rides.php
@@ -52,7 +52,127 @@
               <div class="dropdown-content">
                 <a href="#">Remove</a>
                 <a href="#">Modify</a>
-                <a href="#">Approve passenger</a>
+                <a href="#openModal">Approve passenger</a>
+
+                <!--Boite modale-->
+                <div id="openModal" class="modalDialog">
+
+                    <div><a href="#close" title="Close" class="close">X</a>
+                        
+                        <div id="conteneurPopup">
+                            <h2>New request</h2>
+                            <!--Une requête-->
+                            <div class="aRequest">
+                                <div class="pic">
+                                    <!--Photo de profil + Nom du mec à insérer-->
+                                    <img src="pictures_site/test2.jpg">
+                                </div>
+                                <div class="name">
+                                    <!--Nom du mec-->
+                                    <h3> Cyril Decaud</h3>
+                                </div>
+                                <div class="description">
+                                    <!--Description du mec-->
+                                    <p>Hello j'adore faire chier les gens avec ma soundbox</p>
+                                </div>
+                                <div class="choice">
+                                    <img src="pictures_site/OK.png" action="" method='post'> <!--PHP pour accpeter la requête-->
+                                </div>
+                                <div class="choice">
+                                    <img src="pictures_site/NOK.png" action="" method='post'> <!--PHP pour refuser la requête-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <script>    
+                        if(document.getElementsByClassName('modalDialog').style.opacity != "0"){
+                            document.getElementsByClassName('header').style.opacity = "0";
+                        } else {
+                            document.getElementsByClassName('header').style.opacity = "0";
+                        }
+                    </script>
+
+                </div>
+
+              </div>
+          </div>
+      </div>
+
+        <!-- Un trajet -->
+        <div class="ride">
+            <div class=info>
+                <!-- Info à rentrer en php -->
+                <div class="pic_label">
+                    <img src="Pictures_site/calendar.svg" alt="calendar" width="40"/>
+                    <label for="" style="">25/05/2020</label>
+                </div>
+
+                <div class="end_pic_label">
+                    <img src="Pictures_site/clock.svg" alt="clock" width="40"/>
+                    <label for="">15h03</label>
+                </div>
+            </div>
+
+            <div class=rideResume>
+                <!-- Info à rentrer en php -->
+                <label for="">
+                    Toulouse
+                </label>
+                <img src="Pictures_site/city_sep.svg" alt="city" width="50">
+                <label for="">
+                    Montpellier
+                </label>
+            </div>
+
+            <!-- <div class=vertical_line> </div> -->
+            <a href="profile.php">
+                <div class=pic>
+                    <!-- Info à rentrer en php -->
+                    <img src="Pictures_site/test2.jpg" alt="ppDriver">
+                    <p>Nom du mec</p>
+                </div>
+            </a>
+
+            <div class="dropdown">
+              <img src="Pictures_site/checkbox.svg" alt="Option button" class="dropbtn"></img>
+              <div class="dropdown-content">
+                <a href="#">Remove</a>
+                <a href="#">Modify</a>
+                <a href="#openModal">Approve passenger</a>
+
+                <!--Boite modale-->
+                <div id="openModal" class="modalDialog">
+
+                    <div><a href="#close" title="Close" class="close">X</a>
+                        
+                        <div id="conteneurPopup">
+                            <h2>New request</h2>
+                            <!--Une requête-->
+                            <div class="aRequest">
+                                <div class="pic">
+                                    <!--Photo de profil + Nom du mec à insérer-->
+                                    <img src="pictures_site/test2.jpg">
+                                </div>
+                                <div class="name">
+                                    <!--Nom du mec-->
+                                    <h3> Cyril Decaud</h3>
+                                </div>
+                                <div class="description">
+                                    <!--Description du mec-->
+                                    <p>Hello j'adore faire chier les gens avec ma soundbox</p>
+                                </div>
+                                <div class="choice">
+                                    <img src="pictures_site/OK.png"> <!--PHP pour accepter la requête-->
+                                </div>
+                                <div class="choice">
+                                    <img src="pictures_site/NOK.png"> <!--PHP pour refuser la requête-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
               </div>
           </div>
       </div>
@@ -97,7 +217,18 @@
                 <div class="dropdown-content">
                   <a href="#">Remove</a>
                   <a href="#">Modify</a>
-                  <a href="#">Approve passenger</a>
+                  <a href="#openModal">Approve passenger</a>
+
+                <!--Boite modale-->
+                <div id="openModal" class="modalDialog">
+
+                    <!--Contenu à modifier en fonction de la réussite du formulaire-->
+                    <div>	<a href="#close" title="Close" class="close">X</a>
+                        <h2>Your account was successfully created !</h2>
+                        <p>An email confirmation was sent to your email adress</p>
+                    </div>
+                </div>
+
                 </div>
             </div>
 
@@ -143,53 +274,23 @@
                 <div class="dropdown-content">
                   <a href="#">Remove</a>
                   <a href="#">Modify</a>
-                  <a href="#">Approve passenger</a>
-                </div>
-            </div>
+                  <a href="#openModal">Approve passenger</a>
 
-        </div>
+                    <!--Boite modale-->
+                    <div id="openModal" class="modalDialog">
 
-        <div class=ride>
-              <div class=info>
-                  <!-- Info à rentrer en php -->
-                  <div class="pic_label">
-                      <img src="Pictures_site/calendar.svg" alt="calendar" width="40"/>
-                      <label for="" style="">25/05/2020</label>
-                  </div>
+                        <!--Contenu à modifier en fonction de la réussite du formulaire-->
+                        <div>	<a href="#close" title="Close" class="close">X</a>
+                            <h2>New request</h2>
+                            <div class="passengerRequest">
+                                <div class=pic>
+                                    <img src="Pictures_site/test2.jpg" alt="ppDriver">
+                                    <p>Nom du mec</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                  <div class="end_pic_label">
-                      <img src="Pictures_site/clock.svg" alt="clock" width="40"/>
-                      <label for="">15h03</label>
-                  </div>
-              </div>
-
-              <div class=rideResume>
-                  <!-- Info à rentrer en php -->
-                  <label for="">
-                      Toulouse
-                  </label>
-                  <img src="Pictures_site/city_sep.svg" alt="city" width="50">
-                  <label for="">
-                      Montpellier
-                  </label>
-              </div>
-
-              <!-- <div class=vertical_line> </div> -->
-
-              <a href="profile.php">
-                  <div class=pic>
-                      <!-- Info à rentrer en php -->
-                      <img src="Pictures_site/test2.jpg" alt="ppDriver">
-                      <p>Nom du mec</p>
-                  </div>
-              </a>
-
-              <div class="dropdown">
-                <img src="Pictures_site/checkbox.svg" alt="Option button" class="dropbtn"></img>
-                <div class="dropdown-content">
-                  <a href="#">Remove</a>
-                  <a href="#">Modify</a>
-                  <a href="#">Approve passenger</a>
                 </div>
             </div>
 
