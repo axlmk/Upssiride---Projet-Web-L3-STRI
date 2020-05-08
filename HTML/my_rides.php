0a6ad8c1a8a763fd4fff32fb1d7363917493cd85
Axel G <axel-gauthier@hotmail.fr>
2020-05-08 22:38:10 +0200

    feat: add icons to my_ride titles

--- HTML/my_rides.php
+++ HTML/my_rides.php
@@ -15,7 +15,62 @@
         <link rel="stylesheet" href="style/my_rides.css"/>
     </head>
     <body>
-        <h2>Rides to come</h2>
+
+        <div class="title_div">
+            <img src="Pictures_site/hourglass.svg"/>
+            <h2>Request sent</h2>
+        </div>
+
+        <div class="ride">
+            <div class=info>
+                <!-- Info à rentrer en php -->
+                <div class="pic_label">
+                    <img src="Pictures_site/calendar.svg" alt="calendar" width="40"/>
+                    <label for="" style="">25/05/2020</label>
+                </div>
+
+                <div class="end_pic_label">
+                    <img src="Pictures_site/clock.svg" alt="clock" width="40"/>
+                    <label for="">15h03</label>
+                </div>
+            </div>
+
+            <div class=rideResume>
+                <!-- Info à rentrer en php -->
+                <label for="">
+                    Toulouse
+                </label>
+                <img src="Pictures_site/city_sep.svg" alt="city" width="50">
+                <label for="">
+                    Montpellier
+                </label>
+            </div>
+
+            <!-- <div class=vertical_line> </div> -->
+            <a href="profile.php">
+                <div class=pic>
+                    <!-- Info à rentrer en php -->
+                    <img src="Pictures_site/test2.jpg" alt="ppDriver">
+                    <p>Nom du mec</p>
+                </div>
+            </a>
+
+            <div class="dropdown">
+              <img src="Pictures_site/checkbox.svg" alt="Option button" class="dropbtn"></img>
+              <div class="dropdown-content">
+                <a href="#">Remove</a>
+                <a href="#">Modify</a>
+                <div class="button_container">
+                    <a href="#" onclick="openModal()">Approve passenger</a>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="title_div">
+            <img src="Pictures_site/car.svg"/>
+            <h2>Rides to come</h2>
+        </div>
         <!-- Un trajet -->
 <!-- Debut du div-->
 <!-- foreach ($result as $row) {
@@ -362,7 +417,10 @@
                 </div>
             </div>
 
-        <h2>Rides recently completed</h2>
+        <div class="title_div">
+            <img src="Pictures_site/paper.svg"/>
+            <h2>Rides recently completed</h2>
+        </div>
 
         <div class=ride>
               <div class=info>
