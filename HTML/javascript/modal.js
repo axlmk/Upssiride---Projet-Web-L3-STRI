72618ff8113aa6f7e32cb156e4a6ba00a15ad262
cyril.decaud <cyril.decaud@gmail.com>
2020-05-08 11:05:19 +0200

    ajout popup my_rides.php

--- HTML/javascript/modal.js
+++ HTML/javascript/modal.js
@@ -1,9 +1,11 @@
 function openModal(){
+    document.getElementById("modal").style.visibility = "visible";
     document.getElementById("modal").style.opacity = "1";
-    document.getElementById("modal").style.top = "200px"
+    document.getElementById("modal").style.top = "15%";
 }
 
 function closeModal(){
+    document.getElementById("modal").style.top = "10%";
     document.getElementById("modal").style.opacity = "0";
-    document.getElementById("modal").style.top = "220px"
+    document.getElementById("modal").style.visible = "hidden";
 }
