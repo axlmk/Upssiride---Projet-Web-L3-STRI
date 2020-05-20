8703018e6902b01764eb037fe3728afc108bad47
Axel G <axel-gauthier@hotmail.fr>
2020-05-20 12:24:35 +0200

    feat: push vincent

--- HTML/scripts/apply.php
+++ HTML/scripts/apply.php
@@ -1,19 +1,21 @@
 <?php
-    if (!is_connected()){
+    require_once 'functions/pgsql_connect.php';
+    require_once 'functions/auth.php';
+    /*if (!is_connected()){
         header("Location: connection.php");
-    }
-
+    }*/
+/*
     if (isset($_POST['apply'])){
         $bdd = connect_db();
-        if ($bdd==null){
+        if ($bdd == null){
             return false;
         }
         $id_ride = $_POST['id_ride'];
         $id_passenger = $_SESSION['id'];
-        $query = "INSERT INTO require VALUES($id_passenger,$id_ride,'processing')";
+        $query = "INSERT INTO require VALUES('$id_passenger','$id_ride','processing')";
         $bdd->query($query);
-        header("Location: /HTML/my_rides.php");
+        header("Location: ../my_rides.php");
     }
-    
-    header("Location: /HTML/my_rides.php");
+
+    header("Location: ../my_rides.php");*/
 ?>
