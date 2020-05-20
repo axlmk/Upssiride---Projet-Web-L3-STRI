3005d615ac71d810cca33c1acea69bc6e159ee97
Axel G <axel-gauthier@hotmail.fr>
2020-05-20 13:35:49 +0200

    feat: user can accept passengers

--- HTML/scripts/accept_passenger.php
+++ HTML/scripts/accept_passenger.php
@@ -1,13 +1,14 @@
 <?php
 
-
-    $bdd = connect_db();
-    if ($bdd==null){
-        return false;
+    function accept($id_passenger, $id_ride) {
+        $bdd = connect_db();
+        if ($bdd==null){
+            return false;
+        }
+        echo $id_passenger.$id_ride."wouaw<br>";
+        $query = "INSERT INTO participate VALUES('$id_passenger','$id_ride')";
+        $bdd->query($query);
+        $query2 = "DELETE FROM require WHERE idaccount=$id_passenger AND idride=$id_ride";
+        $bdd->query($query2);
     }
-    $query = "INSERT INTO participate VALUES($id_passenger,$id_ride)";
-    $bdd->query($query);
-    $query2 = "DELETE FROM require WHERE idaccount=$id_passenger AND idride=$id_ride";
-    $bdd->query($query2);
-    header("Location: /HTML/my_rides.php");
 ?>
--- HTML/scripts/apply.php
+++ HTML/scripts/apply.php
@@ -1,10 +1,11 @@
 <?php
-    require_once 'functions/pgsql_connect.php';
-    require_once 'functions/auth.php';
-    /*if (!is_connected()){
+    require_once '../functions/pgsql_connect.php';
+    require_once '../functions/auth.php';
+
+    if (!is_connected()){
         header("Location: connection.php");
-    }*/
-/*
+    }
+
     if (isset($_POST['apply'])){
         $bdd = connect_db();
         if ($bdd == null){
@@ -14,8 +15,8 @@
         $id_passenger = $_SESSION['id'];
         $query = "INSERT INTO require VALUES('$id_passenger','$id_ride','processing')";
         $bdd->query($query);
-        header("Location: ../my_rides.php");
     }
 
-    header("Location: ../my_rides.php");*/
+    header("Location: ../my_rides.php");
+    header("Location: ../my_rides.php");
 ?>
